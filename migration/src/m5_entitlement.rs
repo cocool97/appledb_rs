4b19b324fde9d226381132934054cb7c5c40b8a6
cli-s1n <corentin.liaud@synacktiv.com>
2025-03-26 16:40:27 +0100

    fix: duplicate table in migrations

--- migration/src/m5_entitlement.rs
+++ migration/src/m5_entitlement.rs
@@ -1,11 +1,8 @@
-use sea_orm::sea_query::{ForeignKey, Index};
 use sea_orm_migration::{
     DbErr, MigrationTrait, SchemaManager, async_trait,
     prelude::{ColumnDef, Table},
     sea_orm::{DeriveIden, DeriveMigrationName},
 };
-
-use crate::m4_executable::Executable;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -29,50 +26,7 @@ impl MigrationTrait for Migration {
                     .col(ColumnDef::new(Entitlement::Value).string().not_null())
                     .to_owned(),
             )
-            .await?;
-
-        manager
-            .create_table(
-                Table::create()
-                    .table(ExecutableEntitlement::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(ExecutableEntitlement::ExecutableId)
-                            .not_null()
-                            .integer(),
-                    )
-                    .col(
-                        ColumnDef::new(ExecutableEntitlement::EntitlementId)
-                            .not_null()
-                            .integer(),
-                    )
-                    .foreign_key(
-                        ForeignKey::create()
-                            .from(
-                                ExecutableEntitlement::Table,
-                                ExecutableEntitlement::ExecutableId,
-                            )
-                            .to(Executable::Table, Executable::Id),
-                    )
-                    .foreign_key(
-                        ForeignKey::create()
-                            .from(
-                                ExecutableEntitlement::Table,
-                                ExecutableEntitlement::EntitlementId,
-                            )
-                            .to(Entitlement::Table, Entitlement::Id),
-                    )
-                    .primary_key(
-                        Index::create()
-                            .table(ExecutableEntitlement::Table)
-                            .col(ExecutableEntitlement::ExecutableId)
-                            .col(ExecutableEntitlement::EntitlementId),
-                    )
-                    .to_owned(),
-            )
-            .await?;
-
-        Ok(())
+            .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
@@ -88,11 +42,4 @@ pub enum Entitlement {
     Id,
     Key,
     Value,
-}
-
-#[derive(DeriveIden)]
-pub enum ExecutableEntitlement {
-    Table,
-    ExecutableId,
-    EntitlementId,
 }
