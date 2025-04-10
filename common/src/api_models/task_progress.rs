01e72b88f5935bf86059b95e1617ce35a85b6142
cli-s1n <corentin.liaud@synacktiv.com>
2025-04-10 15:19:17 +0200

    feat(tasks): send start_time + type of task

--- common/src/api_models/task_progress.rs
+++ common/src/api_models/task_progress.rs
@@ -1,14 +1,31 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
 #[derive(Clone, Deserialize, Serialize, ToSchema)]
+pub enum TaskType {
+    PostEntitlements,
+}
+
+#[derive(Clone, Deserialize, Serialize, ToSchema)]
 pub struct TaskProgress {
+    task_type: TaskType,
+    start_time: u64,
     pub done: usize,
     pub total: usize,
 }
 
 impl TaskProgress {
-    pub fn new(total: usize) -> Self {
-        Self { done: 0, total }
+    pub fn new(task_type: TaskType, total: usize) -> Self {
+        Self {
+            task_type,
+            start_time: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap_or(Duration::from_secs(0))
+                .as_secs(),
+            done: 0,
+            total,
+        }
     }
 }
