e6d1c47095c2a54d637ba75f495c7c5ef79187f0
cli-s1n <corentin.liaud@synacktiv.com>
2025-04-15 09:36:13 +0200

    fix: re-gen entities

--- entity/src/entities/device.rs
+++ entity/src/entities/device.rs
@@ -7,9 +7,7 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
-    #[sea_orm(unique)]
     pub model_code: String,
-    #[sea_orm(unique)]
     pub display_name: Option<String>,
 }
 
