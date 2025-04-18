9c6e5390e320186925913ea0ba7c9ed9fabf0e41
cli-s1n <corentin.liaud@synacktiv.com>
2025-04-18 13:22:54 +0200

    feat: add info tooltips

---
+++ web/src/PageInfos.tsx
@@ -0,0 +1,16 @@
+
+export const STATS_PAGE_INFOS = `Displays various server statistics, such as the number of registered devices, discovered entitlements, and detected frameworks.`
+
+export const ENTITLEMENTS_DIFF_INFOS = `Displays entitlements that have been added or removed between two versions of the executable.`
+
+export const ENTITLEMENTS_PER_VERSION_INFOS = `Displays all executables for a given device and version, along with their entitlements. Includes filtering options.`
+
+export const EXECUTABLES_DIFF_INFOS = `Displays executables that have been added or removed between two versions.`
+
+export const EXECUTABLES_FRAMEWORKS_INFOS = `Displays all frameworks used by a given executable for a specific version.`
+
+export const FRAMEWORKS_DIFF_INFOS = `Displays frameworks that have been added or removed between two versions.`
+
+export const LINKED_FRAMEWORKS_INFOS = `Displays all executables using the selected framework in a given version.`
+
+export const TASKS_INFOS = `Displays all tasks currently running on the server, along with their progress status.`
