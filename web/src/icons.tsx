5458c12de7b4881de2a2b75b525a34d3fe1137e3
cli-s1n <corentin.liaud@synacktiv.com>
2025-05-23 15:50:42 +0200

    feat: add optional logo to TitledComponent

--- web/src/icons.tsx
+++ web/src/icons.tsx
@@ -7,9 +7,9 @@ import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
 import SearchIcon from "@mui/icons-material/Search";
 import DifferenceIcon from "@mui/icons-material/Difference";
 
-export const EXECUTABLE_ICON = <LanguageIcon style={{ color: "white" }} />;
-export const ENTITLEMENT_ICON = <KeyIcon style={{ color: "white" }} />;
-export const FRAMEWORK_ICON = <LibraryBooksIcon style={{ color: "white" }} />;
-export const SEARCH_ICON = <SearchIcon style={{ color: "white" }} />;
-export const DIFFERENCE_ICON = <DifferenceIcon style={{ color: "white" }} />;
-export const TASK_ICON = <WorkIcon style={{ color: "white" }} />;
+export const EXECUTABLE_ICON = <LanguageIcon />;
+export const ENTITLEMENT_ICON = <KeyIcon />;
+export const FRAMEWORK_ICON = <LibraryBooksIcon />;
+export const SEARCH_ICON = <SearchIcon />;
+export const DIFFERENCE_ICON = <DifferenceIcon />;
+export const TASK_ICON = <WorkIcon />;
