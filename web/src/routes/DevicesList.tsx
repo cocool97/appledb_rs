7536d2b3c6a8aad1416741feb21ed0d3bc098835
cli-s1n <corentin.liaud@synacktiv.com>
2025-10-27 11:57:14 +0100

    feat: add list of device versions

--- web/src/routes/DevicesList.tsx
+++ web/src/routes/DevicesList.tsx
@@ -1,15 +1,14 @@
 import {
   Box,
+  Collapse,
   IconButton,
   List,
   ListItem,
-  ListItemButton,
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import {
-  API_URL,
   ENTITLEMENTS_SEARCH_ROUTE,
   EXECUTABLES_ROUTE,
   FRAMEWORKS_ROUTE,
@@ -26,6 +25,7 @@ import { Device } from "../types/device";
 
 const DevicesList = () => {
   const [devices, setDevices] = useState<Device[]>([]);
+  const [openDevices, setOpenDevices] = useState<Record<string, boolean>>({});
 
   useEffect(() => {
     fetch(`${GET_ALL_DEVICES_ENDPOINT}`)
@@ -44,17 +44,25 @@ const DevicesList = () => {
       });
   }, []);
 
+  const toggleDevice = (model_code: string) => {
+    setOpenDevices((prev) => ({
+      ...prev,
+      [model_code]: !prev[model_code],
+    }));
+  };
+
   return (
     <Box>
       <List sx={{ width: "100%" }} component="nav">
-        {devices.map((device) => {
-          return (
+        {devices.map((device) => (
+          <React.Fragment key={device.model_code}>
             <ListItem
-              key={device.model_code}
+              onClick={() => toggleDevice(device.model_code)}
               sx={{
                 backgroundColor: "#555555",
                 borderRadius: "5px",
-                marginBottom: "10px",
+                cursor: "pointer",
+                "&:hover": { backgroundColor: "#666666" },
               }}
             >
               <ListItemIcon>{DEVICE_ICON}</ListItemIcon>
@@ -65,20 +73,52 @@ const DevicesList = () => {
                 { route: ENTITLEMENTS_SEARCH_ROUTE, icon: ENTITLEMENT_ICON },
                 { route: EXECUTABLES_ROUTE, icon: EXECUTABLE_ICON },
                 { route: FRAMEWORKS_ROUTE, icon: FRAMEWORK_ICON },
-              ].map((elem) => {
-                return (
-                  <IconButton
-                    component={Link}
-                    to={`${elem.route}?device_id=${device.id}`}
-                    sx={{ maxWidth: "fit-content" }}
-                  >
-                    {elem.icon}
-                  </IconButton>
-                );
-              })}
+              ].map((elem) => (
+                <IconButton
+                  key={elem.route}
+                  component={Link}
+                  to={`${elem.route}?device_id=${device.id}`}
+                  sx={{ maxWidth: "fit-content" }}
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  {elem.icon}
+                </IconButton>
+              ))}
             </ListItem>
-          );
-        })}
+
+            <Collapse
+              in={!!openDevices[device.model_code]}
+              timeout="auto"
+              unmountOnExit
+            >
+              <List component="div" disablePadding sx={{ pl: 8 }}>
+                {device.versions.map((version) => (
+                  <ListItem key={version.id} sx={{ pl: 4 }}>
+                    <ListItemText primary={version.version} />
+                    {[
+                      {
+                        route: ENTITLEMENTS_SEARCH_ROUTE,
+                        icon: ENTITLEMENT_ICON,
+                      },
+                      { route: EXECUTABLES_ROUTE, icon: EXECUTABLE_ICON },
+                      { route: FRAMEWORKS_ROUTE, icon: FRAMEWORK_ICON },
+                    ].map((elem) => (
+                      <IconButton
+                        key={elem.route}
+                        component={Link}
+                        to={`${elem.route}?device_id=${device.id}&device_version_id=${version.id}`}
+                        sx={{ maxWidth: "fit-content" }}
+                        onClick={(e) => e.stopPropagation()}
+                      >
+                        {elem.icon}
+                      </IconButton>
+                    ))}
+                  </ListItem>
+                ))}
+              </List>
+            </Collapse>
+          </React.Fragment>
+        ))}
       </List>
     </Box>
   );
---
+++ web/src/types/operating_system_version.tsx
@@ -0,0 +1,6 @@
+export interface OperatingSystemVersion {
+  id: number;
+  version: string;
+  operating_system_id: number;
+  device_id: number;
+}
